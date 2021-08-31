10a2059c481e379b1bdff5e1b43a6d438e356b22
lehaidang <H-SKY@DESKTOP-NBVSKA9>
2021-08-31 14:42:13 +0700

    baitapgiaipt

--- BT2/Controllers/GiaiphuongtrinhController.cs
+++ BT2/Controllers/GiaiphuongtrinhController.cs
@@ -8,9 +8,20 @@ namespace BT2.Controllers
 {
     public class GiaiphuongtrinhController : Controller
     {
-        // GET: Giaiphuongtrinh
+        // GET: giaiphuongtrinh
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult GPT()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GPT(double hesoA, double hesoB)
+        {
+            double x = gpt.GiaiPhuongTrinhBacNhat(hesoA, hesoB);
+            ViewBag.nghiemPT = x;
             return View();
         }
     }
